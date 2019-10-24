5a23babb55e6a1211f6978bf036acc631dcf0719
Peter Valdez <peter@nycmesh.net>
2019-10-24 13:00:57 -0400

    Set static path to empty string

--- vote/app.py
+++ vote/app.py
@@ -9,7 +9,7 @@ option_a = os.getenv('OPTION_A', "Cats")
 option_b = os.getenv('OPTION_B', "Dogs")
 hostname = socket.gethostname()
 
-app = Flask(__name__, static_url_path='/vote')
+app = Flask(__name__, static_url_path='')
 
 
 def get_redis():
